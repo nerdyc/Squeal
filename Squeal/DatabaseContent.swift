1aafd1cf0a0f0682304652512a4df842678711d0
Christian Niles <christian@nerdyc.com>
2014-08-17 21:07:44 -0700

    INSERT statements can be prepared via Database.prepareInsertInto(...)
    #4

--- Squeal/DatabaseContent.swift
+++ Squeal/DatabaseContent.swift
@@ -9,7 +9,7 @@ extension Database {
     // -----------------------------------------------------------------------------------------------------------------
     // MARK:  Insert
     
-    public func insertRow(tableName:String, columns:[String], values:[Bindable?], error:NSErrorPointer) -> Int64? {
+    public func prepareInsertInto(tableName:String, columns:[String], error:NSErrorPointer) -> Statement? {
         var sqlFragments = ["INSERT INTO"]
         sqlFragments.append(escapeIdentifier(tableName))
         sqlFragments.append("(")
@@ -20,8 +20,12 @@ extension Database {
         sqlFragments.append(join(",", columns.map { _ in "?" }))
         sqlFragments.append(")")
         
+        return prepareStatement(join(" ", sqlFragments), error: error)
+    }
+    
+    public func insertRow(tableName:String, columns:[String], values:[Bindable?], error:NSErrorPointer) -> Int64? {
         var rowId : Int64?
-        if let statement = prepareStatement(join(" ", sqlFragments), error: error) {
+        if let statement = prepareInsertInto(tableName, columns:columns, error: error) {
             if statement.bind(values, error: error) {
                 if statement.execute(error) {
                     rowId = lastInsertedRowId
