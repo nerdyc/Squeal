2a401415a9b858e319d6cebce1f9ecde175727b4
Christian Niles <christian@nerdyc.com>
2014-08-27 23:06:14 -0700

    Added doc comments for content helpers
    E.g. INSERT, SELECT, UPDATE, and DELETE.

--- Squeal/DatabaseContent.swift
+++ Squeal/DatabaseContent.swift
@@ -23,6 +23,17 @@ extension Database {
         return prepareStatement(join(" ", sqlFragments), error: error)
     }
     
+    /// Inserts a table row. This is a helper for executing an INSERT INTO statement.
+    ///
+    /// :param: tableName   The name of the table to insert into.
+    /// :param: columns     The column names of the values to insert.
+    /// :param: values      The values to insert. The values in this array must be in the same order as the respective
+    ///                     `columns`.
+    /// :param: error       An error pointer.
+    ///
+    /// :returns:   The id of the inserted row. sqlite assigns each row a 64-bit ID, even if the primary key is not an
+    ///             INTEGER value. `nil` is returned when an error occurs.
+    ///
     public func insertInto(tableName:String, columns:[String], values:[Bindable?], error:NSErrorPointer) -> Int64? {
         var rowId : Int64?
         if let statement = prepareInsertInto(tableName, columns:columns, error: error) {
@@ -35,7 +46,16 @@ extension Database {
         }
         return rowId
     }
-    
+
+    /// Inserts a table row. This is a helper for executing an INSERT INTO statement.
+    ///
+    /// :param: tableName   The name of the table to insert into.
+    /// :param: values      The values to insert, keyed by the column name.
+    /// :param: error       An error pointer.
+    ///
+    /// :returns:   The id of the inserted row. sqlite assigns each row a 64-bit ID, even if the primary key is not an
+    ///             INTEGER value. `nil` is returned when an error occurs.
+    ///
     public func insertInto(tableName:String, values valuesDictionary:[String:Bindable?], error:NSErrorPointer) -> Int64? {
         var columns = [String]()
         var values = [Bindable?]()
@@ -112,6 +132,27 @@ extension Database {
         return statement
     }
     
+    /// Selects table rows and iterates over them. This is a helper for executing a SELECT statement, and reading the
+    /// results.
+    ///
+    /// Results are read by the `collector` block. The block will be invoked for each row of the result set, and is
+    /// expected to return a value read from the row. It will be provided a Statement, from which the row can be read.
+    ///
+    /// :param: from        The name of the table to select from, including any JOIN clauses.
+    /// :param: columns     The columns to select. These are not escaped, and can contain expressions. If nil, all
+    ///                     columns are returned (e.g. '*').
+    /// :param: whereExpr   The WHERE clause. If nil, then all rows are returned.
+    /// :param: groupBy     The GROUP BY expression.
+    /// :param: having      The HAVING clause.
+    /// :param: orderBy     The ORDER BY clause.
+    /// :param: limit       The LIMIT.
+    /// :param: offset      The OFFSET.
+    /// :param: parameters  An array of parameters to bind to the statement.
+    /// :param: error       An error pointer.
+    /// :param: collector   A block used to read each row.
+    ///
+    /// :returns:   An array of all values read, or nil if an error occurs.
+    ///
     public func selectFrom<T>(from:        String,
                               columns:     [String]? = nil,
                               whereExpr:   String? = nil,
@@ -144,6 +185,17 @@ extension Database {
         }
     }
 
+    /// Counts rows in a table. This is a helper for executing a SELECT count(...) FROM statement and reading the
+    /// result.
+    ///
+    /// :param: from        The name of the table to select from, including any JOIN clauses.
+    /// :param: columns     The columns to count. If nil, then 'count(*)' is returned.
+    /// :param: whereExpr   The WHERE clause. If nil, then all rows are returned.
+    /// :param: parameters  An array of parameters to bind to the statement.
+    /// :param: error       An error pointer.
+    ///
+    /// :returns:   The number of rows counted, or nil if an error occurs.
+    ///
     public func countFrom(from:        String,
                           columns:     [String]? = nil,
                           whereExpr:   String? = nil,
@@ -196,6 +248,17 @@ extension Database {
         return prepareStatement(join(" ", fragments), error: error)
     }
     
+    /// Updates table rows. This is a helper for executing an UPDATE ... SET ... WHERE statement.
+    ///
+    /// :param: tableName   The name of the table to update.
+    /// :param: columns     The columns to update.
+    /// :param: values      The updated values. These values must be in the same order as the columns.
+    /// :param: whereExpr   A WHERE clause to select which rows to update. If nil, all rows are updated.
+    /// :param: parameters  Parameters to the WHERE clause.
+    /// :param: error       An error pointer.
+    ///
+    /// :returns:   The number of rows updated, or nil if an error occurs.
+    ///
     public func update(tableName:   String,
                        columns:     [String],
                        values:      [Bindable?],
@@ -215,6 +278,16 @@ extension Database {
         return numberOfChangedRows
     }
     
+    /// Updates table rows. This is a helper for executing an UPDATE ... SET ... WHERE statement.
+    ///
+    /// :param: tableName   The name of the table to update.
+    /// :param: set         The updated values, keyed by column names.
+    /// :param: whereExpr   A WHERE clause to select which rows to update. If nil, all rows are updated.
+    /// :param: parameters  Parameters to the WHERE clause.
+    /// :param: error       An error pointer.
+    ///
+    /// :returns:   The number of rows updated, or nil if an error occurs.
+    ///
     public func update(tableName:   String,
                        set:         [String:Bindable?],
                        whereExpr:   String? = nil,
@@ -247,6 +320,15 @@ extension Database {
         return prepareStatement(join(" ", fragments), error: error)
     }
 
+    /// Deletes table rows. This is a helper for executing an DELETE FROM ... WHERE statement.
+    ///
+    /// :param: tableName   The name of the table to update.
+    /// :param: whereExpr   A WHERE clause to select which rows to update. If nil, all rows are deleted.
+    /// :param: parameters  Parameters to the WHERE clause.
+    /// :param: error       An error pointer.
+    ///
+    /// :returns:   The number of rows removed, or nil if an error occurs.
+    ///
     public func deleteFrom(tableName:   String,
                            whereExpr:   String? = nil,
                            parameters:  [Bindable?] = [],
