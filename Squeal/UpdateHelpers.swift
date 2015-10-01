8027a9449de48b43fbb604a2b287787de9212a60
Christian Niles <christian@nerdyc.com>
2015-10-01 11:38:24 -0700

    Added helper for UPDATE statements that use SQL expressions
    Existing helpers didn't include support for using SQL expressions to set new values.

--- Squeal/UpdateHelpers.swift
+++ Squeal/UpdateHelpers.swift
@@ -2,22 +2,51 @@ import Foundation
 
 public extension Database {
     
+    public func prepareUpdate(tableName: String, setExpr:String, whereExpr:String? = nil, parameters: [Bindable?] = []) throws -> Statement {
+        var fragments = ["UPDATE", escapeIdentifier(tableName), "SET", setExpr]
+        
+        if whereExpr != nil {
+            fragments.append("WHERE")
+            fragments.append(whereExpr!)
+        }
+        
+        let statement = try prepareStatement(fragments.joinWithSeparator(" "))
+        if parameters.count > 0 {
+            try statement.bind(parameters)
+        }
+        return statement
+    }
+
+    /// Updates table rows using a dynamic SQL expression. This is useful when you would like to use
+    /// SQL functions or operators to update the values of particular rows.
+    ///
+    /// :param: tableName   The name of the table to update.
+    /// :param: setExpr     The SQL expression used to calculate the updated values.
+    /// :param: whereExpr   A WHERE clause to select which rows to update. If nil, all rows are updated.
+    /// :param: parameters  Parameters to the SET and WHERE clauses.
+    ///
+    /// :returns:   The number of rows updated.
+    ///
+    public func update(tableName: String, setExpr:String, whereExpr:String? = nil, parameters: [Bindable?] = []) throws -> Int {
+        let statement = try prepareUpdate(tableName,
+                                          setExpr:setExpr,
+                                          whereExpr:whereExpr,
+                                          parameters:parameters)
+        try statement.execute()
+        
+        return self.numberOfChangedRows
+    }
+    
     public func prepareUpdate(tableName:   String,
                               columns:     [String],
                               whereExpr:   String? = nil) throws -> Statement {
         
         let columnsToSet = columns.map { escapeIdentifier($0) + " = ?" }.joinWithSeparator(", ")
-        var fragments = ["UPDATE", escapeIdentifier(tableName), "SET", columnsToSet]
-                                
-        if whereExpr != nil {
-            fragments.append("WHERE")
-            fragments.append(whereExpr!)
-        }
-
-        return try prepareStatement(fragments.joinWithSeparator(" "))
+        return try prepareUpdate(tableName, setExpr:columnsToSet, whereExpr:whereExpr)
     }
     
-    /// Updates table rows. This is a helper for executing an UPDATE ... SET ... WHERE statement.
+    /// Updates table rows with the given values. This is a helper for executing an
+    /// `UPDATE ... SET ... WHERE` statement when all updated values are provided.
     ///
     /// :param: tableName   The name of the table to update.
     /// :param: columns     The columns to update.
@@ -40,7 +69,8 @@ public extension Database {
         return self.numberOfChangedRows
     }
     
-    /// Updates table rows. This is a helper for executing an UPDATE ... SET ... WHERE statement.
+    /// Updates table rows with the given values. This is a helper for executing an
+    /// `UPDATE ... SET ... WHERE` statement when all updated values are provided.
     ///
     /// :param: tableName   The name of the table to update.
     /// :param: set         The updated values, keyed by column names.
